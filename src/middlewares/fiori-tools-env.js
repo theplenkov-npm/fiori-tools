0f2a9da917b117c0f5497ab867c1443212f690c9
Petr Plenkov <petr.plenkov@booking.com>
2020-11-24 08:36:32 +0100

    initial commit

---
+++ src/middlewares/fiori-tools-env.js
@@ -0,0 +1,18 @@
+module.exports = function ({ resources, middlewareUtil, options }) {
+  let { configuration } = options;
+
+  let { no_proxy } = configuration;
+
+  process.env.NO_PROXY = [no_proxy]
+    .flat()
+    .reduce(
+      (no_proxy, host) => `${no_proxy},${host}`,
+      process.env.NO_PROXY || process.env.no_proxy || ""
+    );
+
+  console.log(process.env.NO_PROXY);
+
+  return (req, res, next) => {
+    next();
+  };
+};
---
+++ src/middlewares/fiori-tools-proxy-client.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+module.exports = function ({ resources, middlewareUtil, options }) {
+  const app = express();
+
+  let { configuration } = options;
+
+  let { backend } = configuration;
+
+  [backend].flat().forEach(({ path, client }) => {
+    app.use(`${path}*`, (req, res, next) => {
+      let oUrl = new URL(req.originalUrl, "http://localhost");
+      oUrl.searchParams.set("sap-client", client);
+      req.originalUrl = oUrl.toString().replace("http://localhost", "");
+
+      // req.query["sap-client"] = client;
+      // req.headers["sap-client"] = client;
+      // req.headers.cookie = req.headers.cookie.replace(
+      //   /(?<=sap-client=).*(?=;)|(?<=sap-client=).*$/,
+      //   client
+      // );
+      next();
+    });
+    console.log(`Setting up client ${client} for requests to ${path}`);
+  });
+
+  return app;
+};
