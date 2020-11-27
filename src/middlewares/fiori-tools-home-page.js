891f63a07ba01585229abc007784cbd5ee292987
Petr Plenkov <petr.plenkov@booking.com>
2020-11-27 17:48:00 +0000

    home page: redirect with queries

--- src/middlewares/fiori-tools-home-page.js
+++ src/middlewares/fiori-tools-home-page.js
@@ -7,9 +7,18 @@ module.exports = function ({ options }) {
 
   let { home_page } = configuration;
 
+  // only needed to create URL
+  const dummy = "http://localhost";
+
   // change URL parameters
   app.get("/", (req, res) => {
-    res.redirect(home_page);
+    // create dummy url to reuse URL api
+    let oUrl = new URL(home_page, dummy);
+    //fill query
+    Object.entries(req.query).forEach((query) =>
+      oUrl.searchParams.set(query[0], query[1])
+    );
+    res.redirect(oUrl.toString().replace(dummy, ""));
   });
 
   return app;
