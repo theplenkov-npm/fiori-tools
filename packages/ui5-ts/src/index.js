b9f1f2858f1558abb7e5da94d896425f22ee5982
Petr Plenkov <petr.plenkov@booking.com>
2023-07-25 19:03:09 +0000

    chore(ui5-ts): using dotenv

--- packages/ui5-ts/src/index.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env -S node --loader ts-node/esm
-import '@ui5/cli/bin/ui5.cjs';
---
+++ packages/ui5-ts/src/ui5-ts.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env -S node --loader ts-node/esm -r dotenv/config
+import '@ui5/cli/bin/ui5.cjs';
